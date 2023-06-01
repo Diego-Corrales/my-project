9ea2953ce5a37ece3eff09df324a87c656f5cabd
Diego.Corrales <docarquitectura@hotmail.com>
2023-06-01 17:37:19 -0300

    push desde casa

--- app/components/header.js
+++ app/components/header.js
@@ -16,13 +16,13 @@ const links = [{
 
 export function Header() {
     return (
-        <header className='flex w-100 h-40 gap-40 bg-cyan-300 justify-around items-center rounded-3xl border-cyan-600 border-8'>
+        <header className='flex w-100 h-40 gap-40 bg-cyan-300 justify-around items-center rounded-3xl border-cyan-800 border-8'>
           <div className=''>
             <h2 className='text-4xl text-black'>Diego Omar Corrales</h2>
             <h3 className='text-2xl text-black'>Front-end Developer - Buenos Aires/Argentina</h3>
           </div>
           <nav>
-            <ul className='flex items-center bg-cyan-600 text-white h-20 border-solid rounded-3xl border-white border-2 p-10 text-2xl gap-10'>
+            <ul className='flex items-center bg-cyan-800 text-white h-20 border-solid rounded-3xl border-white border-2 p-10 text-2xl gap-10'>
               {links.map(({ label, route }) => (
                 <li key={route}>
                   <Link href={route}>
